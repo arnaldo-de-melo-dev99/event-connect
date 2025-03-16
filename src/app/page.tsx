5ce56272ecb4a50a8742ce2d13eb1f9ca48da753
Arnaldo de Melo <money.dev.solution@gmail.com>
2025-03-16 14:45:06 +0100

    commit-file

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl bg-blue-700 font-bold underline caret-blue-900 block w-2 h-2 border-b-amber-500">
+      Hello world!
+    </h1>  
+  )
+}
